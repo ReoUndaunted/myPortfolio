1512579b95fddf8374f44c6930600336a503a227
Raphael E. Ovonlen <reoundaunted@gmail.com>
2023-03-31 17:02:13 +0100

    my First commit

---
+++ Portfolio Website/js/app.js
@@ -0,0 +1,59 @@
+//note: var boy = [32, "Abel", "ss3"]
+// function TODO(boy) {
+//   boy = boy.pop[2];
+//   return boy;
+// }
+// console.log(TODO)
+
+// let a = 5;
+// let b = '16';
+// let c = a + b;
+// if (a !== b && c >= a) {
+//   console.log('positive')
+// } else {
+//   console.log('negative')
+// }
+
+//note: Types of operators
+//! addition =  +;
+//* subtraction = -;
+//! multiplication = *;
+//* division = /;
+//! modulo = % (here, this means reminder not percentage);
+//* assignment = =;
+//! equator  =  ==;
+//* strict equator = ===;
+
+//note: Logical Operators
+// and = &&
+// or = ||
+// greater than = >
+// lesser than = <
+// greater than or equals = >=
+//lesser than or equals = <=
+// not equals = !=
+// strictly not equals = !==s
+let e = '';
+let d = "Almost there"
+for (let i = 0; i < 10; i++)
+  if (i === 5) {
+    return e += i
+    console.log(d);
+  }
+console.log(e.push(i))
+
+
+// .push()
+// .pop()
+// .splice()
+// .slice()
+function addNumbers(number1, number2) {
+  return number1 + number2
+}
+console.log(addNumbers(5, 7));
+console.log(addNumbers(5, -7));
+console.log(addNumbers(5, 9));
+
+
+
+
